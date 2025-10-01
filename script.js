5407c438a59d8c0ca3cb38d2cb9ec457aca259aa
Arm4nd7 <armando.estevezt@gmail.com>
2025-10-01 12:49:32 -0500

    Updates

---
+++ script.js
@@ -0,0 +1,50 @@
+let createButton = document.querySelector("button");
+let description = document.getElementById("description");
+
+let createCards = [];
+let createDescription = [];
+
+createButton.addEventListener("click", newCards);
+//Creacion de contenedor y agregacion de titulo y parrafo
+function newCards(){
+    let newCard = document.createElement("div");
+    newCard.classList.add("card-div");
+    createCards.push(newCard);
+    description.remove();
+    document.getElementById("tarjetas").appendChild(newCard);
+    titleH3(newCard);
+    descriptionP(newCard);
+    buttonDiv(newCard);
+}
+//creacion de titulo
+function titleH3(element){
+    let newTitle = document.createElement("h3");
+    newTitle.textContent = "Titulo h3";
+    createDescription.push(newTitle);
+    element.appendChild(newTitle);
+}
+// ceracion de parrafo
+function descriptionP(element){
+    let newP = document.createElement("p");
+    newP.textContent = "Nueva descripcion para esta tarjeta";
+    createDescription.push(newP);
+    element.appendChild(newP);
+}
+
+function buttonDiv(element) {
+    let newButtonDiv = document.createElement("button");
+    newButtonDiv.textContent = "Eliminar";
+    createDescription.push(newButtonDiv);
+    element.appendChild(newButtonDiv);
+
+    newButtonDiv.addEventListener("click", () =>{
+    if(createDescription.length >0 ){
+        let removeBtn = createCards.pop();
+        removeBtn.remove();
+    }
+    });
+}
+
+
+
+
